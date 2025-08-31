0b1179d13e367fe9356dd67eabac16a9084a42a1
Oscar Blanco <oscarmb86@hotmail.com>
2025-08-30 23:51:34 -0600

    preparing for routes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-const args = process.argv.slice(2);
+import app from './app.js'
+const PORT = process.env.PORT || 3000
 
-let min = 1
-let max = 100
-
-console.log('values in args',args)
-
-if (args.length >=2) {
-    const parsedMin = parseInt(args[0], 10);
-    const parsedMax = parseInt(args[1], 10);
-
-    if (!isNaN(parsedMin) && !isNaN(parsedMax) && parsedMin < parsedMax) {
-        min = parsedMin
-        max = parsedMax
-    } else {
-        console.log('Rango error')
-    }
-    
-}
-
-const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
-
-console.log(`Numero aleatorio entre ${min} y ${max} es: ${randomNumber}` )
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
